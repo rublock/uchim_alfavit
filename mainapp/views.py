34c8ad6cc81b3a10d0de5580b342d47fe4eb9e00
rublock <hypermail@yandex.ru>
2023-10-16 16:57:56 +0300

    static n view

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
 
-from django.shortcuts import render
+import os
+
 
 def home_page(request):
-    return render(request, "mainapp/home_page.html")
+    folder_path = 'mainapp/static/audio'
+    files = os.listdir(folder_path)
+
+    audio_files = []
+    chars = []
+
+    for i in files:
+        audio_files.append(i)
+        chars.append(i.split('.')[0])
+
+    main_list = zip(audio_files, chars)
+
+    print(main_list)
+
+    return render(request, "mainapp/home_page.html", {
+        'main_list': main_list,
+    })
